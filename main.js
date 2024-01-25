1115f80d1b2a2b284b73d54142ebebc71dd613f1
Bozo2023 <ulrob1.0@email.com>
2024-01-25 16:35:50 +0100

    demain

--- main.js
+++ main.js
@@ -6,10 +6,23 @@ const griglia3Html=document.getElementById("griglia3");
 const difficoltaHtml=document.getElementById("grado");
 const enterveHtml=document.getElementById("enter");
 
-
 enterveHtml.addEventListener("click",function(){
+   grigliaHtml.innerHTML=""
+
+   //creiamo l'array dellle bombe
+   let Bomb=[]
+   do{
+
+      let randomNum= random(100)   //inseriamo nella griglia e verifichiamo che non superi il numero delle celle
+      if( ! Bomb.includes(randomNum)){
+         Bomb.push(randomNum)
+      }
+   } while(  Bomb.length< 16 )
+   console.log(Bomb)
+
+
    if(difficoltaHtml.value==="cento"){
-          for(let i = 1; i < 100;i ++){
+          for(let i = 1; i <= 100;i ++){
                let box = document.createElement("div") // questo per creare un sigolo div
                   // adesso accosicamo una classe con la  funzione box.classlist
                   box.classList.add("box")
@@ -23,24 +36,30 @@ enterveHtml.addEventListener("click",function(){
       for(let i = 0; i < 81;i ++){
           let box = document.createElement("div") // questo per creare un sigolo div
          // adesso accosicamo una classe con la  funzione box.classlist
-          box.classList.add("box")
+          box.classList.add("box2")
             //Permet d'ecrire dans la box
             box.innerText=i
             //mettiamo la div di nome box all'interno della griglia
             grigliaHtml.append(box)
       }
-   } else (difficoltaHtml.value==="quarantanove")
+   } else if (difficoltaHtml.value==="quarantanove")
             for(let i = 0; i < 49;i ++){
                 let box = document.createElement("div") // questo per creare un sigolo div
                         // adesso accosicamo una classe con la  funzione box.classlist
-                box.classList.add("box")
+                box.classList.add("box3")
                //Permet d'ecrire dans la box
              box.innerText=i
                //mettiamo la div di nome box all'interno della griglia
             grigliaHtml.append(box)
       }
+      if ( Bomb.includes)
 })
      
+/*funzione random */
+function random(max){
+   return Math.floor(Math.random()*max)+1
+}
+
 
 
 
